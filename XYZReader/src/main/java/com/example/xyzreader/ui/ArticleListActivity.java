9e9e37f1efd069da6210c1373c2116dce5540275
Michael Lorenz <michael.lorenz@intive-kupferwerk.com>
2016-12-13 09:48:18 +0100

    apply color, fix main layout

--- XYZReader/src/main/java/com/example/xyzreader/ui/ArticleListActivity.java
+++ XYZReader/src/main/java/com/example/xyzreader/ui/ArticleListActivity.java
@@ -14,8 +14,6 @@ import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.StaggeredGridLayoutManager;
 import android.support.v7.widget.Toolbar;
 import android.text.format.DateUtils;
-import android.util.TypedValue;
-import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
@@ -31,147 +29,141 @@ import com.example.xyzreader.data.UpdaterService;
  * touched, lead to a {@link ArticleDetailActivity} representing item details. On tablets, the
  * activity presents a grid of items as cards.
  */
-public class ArticleListActivity extends ActionBarActivity implements
-        LoaderManager.LoaderCallbacks<Cursor> {
+public class ArticleListActivity extends ActionBarActivity
+      implements LoaderManager.LoaderCallbacks<Cursor> {
 
-    private Toolbar mToolbar;
-    private SwipeRefreshLayout mSwipeRefreshLayout;
-    private RecyclerView mRecyclerView;
+   public static class ViewHolder extends RecyclerView.ViewHolder {
+      public TextView subtitleView;
+      public DynamicHeightNetworkImageView thumbnailView;
+      public TextView titleView;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_article_list);
+      public ViewHolder(View view) {
+         super(view);
+         thumbnailView = (DynamicHeightNetworkImageView) view.findViewById(R.id.thumbnail);
+         titleView = (TextView) view.findViewById(R.id.article_title);
+         subtitleView = (TextView) view.findViewById(R.id.article_subtitle);
+      }
+   }
 
-        mToolbar = (Toolbar) findViewById(R.id.toolbar);
+   private class Adapter extends RecyclerView.Adapter<ViewHolder> {
+      private Cursor mCursor;
 
+      public Adapter(Cursor cursor) {
+         mCursor = cursor;
+      }
 
-        final View toolbarContainerView = findViewById(R.id.toolbar_container);
+      @Override
+      public int getItemCount() {
+         return mCursor.getCount();
+      }
 
-        mSwipeRefreshLayout = (SwipeRefreshLayout) findViewById(R.id.swipe_refresh_layout);
+      @Override
+      public long getItemId(int position) {
+         mCursor.moveToPosition(position);
+         return mCursor.getLong(ArticleLoader.Query._ID);
+      }
 
-        mRecyclerView = (RecyclerView) findViewById(R.id.recycler_view);
-        getLoaderManager().initLoader(0, null, this);
+      @Override
+      public void onBindViewHolder(ViewHolder holder, int position) {
+         mCursor.moveToPosition(position);
+         holder.titleView.setText(mCursor.getString(ArticleLoader.Query.TITLE));
+         holder.subtitleView.setText(DateUtils.getRelativeTimeSpanString(
+               mCursor.getLong(ArticleLoader.Query.PUBLISHED_DATE), System.currentTimeMillis(),
+               DateUtils.HOUR_IN_MILLIS, DateUtils.FORMAT_ABBREV_ALL)
+               .toString() + " by " + mCursor.getString(ArticleLoader.Query.AUTHOR));
+         holder.thumbnailView.setImageUrl(mCursor.getString(ArticleLoader.Query.THUMB_URL),
+               ImageLoaderHelper.getInstance(ArticleListActivity.this)
+                     .getImageLoader());
+         holder.thumbnailView.setAspectRatio(mCursor.getFloat(ArticleLoader.Query.ASPECT_RATIO));
+      }
 
-        if (savedInstanceState == null) {
-            refresh();
-        }
-    }
-
-    private void refresh() {
-        startService(new Intent(this, UpdaterService.class));
-    }
-
-    @Override
-    protected void onStart() {
-        super.onStart();
-        registerReceiver(mRefreshingReceiver,
-                new IntentFilter(UpdaterService.BROADCAST_ACTION_STATE_CHANGE));
-    }
-
-    @Override
-    protected void onStop() {
-        super.onStop();
-        unregisterReceiver(mRefreshingReceiver);
-    }
-
-    private boolean mIsRefreshing = false;
-
-    private BroadcastReceiver mRefreshingReceiver = new BroadcastReceiver() {
-        @Override
-        public void onReceive(Context context, Intent intent) {
-            if (UpdaterService.BROADCAST_ACTION_STATE_CHANGE.equals(intent.getAction())) {
-                mIsRefreshing = intent.getBooleanExtra(UpdaterService.EXTRA_REFRESHING, false);
-                updateRefreshingUI();
+      @Override
+      public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+         View view = getLayoutInflater().inflate(R.layout.list_item_article, parent, false);
+         final ViewHolder vh = new ViewHolder(view);
+         view.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+               startActivity(new Intent(Intent.ACTION_VIEW,
+                     ItemsContract.Items.buildItemUri(getItemId(vh.getAdapterPosition()))));
             }
-        }
-    };
+         });
+         return vh;
+      }
+   }
 
-    private void updateRefreshingUI() {
-        mSwipeRefreshLayout.setRefreshing(mIsRefreshing);
-    }
+   private boolean mIsRefreshing = false;
+   private RecyclerView mRecyclerView;
+   private SwipeRefreshLayout mSwipeRefreshLayout;
+   private BroadcastReceiver mRefreshingReceiver = new BroadcastReceiver() {
+      @Override
+      public void onReceive(Context context, Intent intent) {
+         if (UpdaterService.BROADCAST_ACTION_STATE_CHANGE.equals(intent.getAction())) {
+            mIsRefreshing = intent.getBooleanExtra(UpdaterService.EXTRA_REFRESHING, false);
+            updateRefreshingUI();
+         }
+      }
+   };
+   private Toolbar mToolbar;
 
-    @Override
-    public Loader<Cursor> onCreateLoader(int i, Bundle bundle) {
-        return ArticleLoader.newAllArticlesInstance(this);
-    }
+   @Override
+   public Loader<Cursor> onCreateLoader(int i, Bundle bundle) {
+      return ArticleLoader.newAllArticlesInstance(this);
+   }
 
-    @Override
-    public void onLoadFinished(Loader<Cursor> cursorLoader, Cursor cursor) {
-        Adapter adapter = new Adapter(cursor);
-        adapter.setHasStableIds(true);
-        mRecyclerView.setAdapter(adapter);
-        int columnCount = getResources().getInteger(R.integer.list_column_count);
-        StaggeredGridLayoutManager sglm =
-                new StaggeredGridLayoutManager(columnCount, StaggeredGridLayoutManager.VERTICAL);
-        mRecyclerView.setLayoutManager(sglm);
-    }
+   @Override
+   public void onLoadFinished(Loader<Cursor> cursorLoader, Cursor cursor) {
+      Adapter adapter = new Adapter(cursor);
+      adapter.setHasStableIds(true);
+      mRecyclerView.setAdapter(adapter);
+      int columnCount = getResources().getInteger(R.integer.list_column_count);
+      StaggeredGridLayoutManager sglm =
+            new StaggeredGridLayoutManager(columnCount, StaggeredGridLayoutManager.VERTICAL);
+      mRecyclerView.setLayoutManager(sglm);
+   }
 
-    @Override
-    public void onLoaderReset(Loader<Cursor> loader) {
-        mRecyclerView.setAdapter(null);
-    }
+   @Override
+   public void onLoaderReset(Loader<Cursor> loader) {
+      mRecyclerView.setAdapter(null);
+   }
 
-    private class Adapter extends RecyclerView.Adapter<ViewHolder> {
-        private Cursor mCursor;
+   @Override
+   protected void onCreate(Bundle savedInstanceState) {
+      super.onCreate(savedInstanceState);
+      setContentView(R.layout.activity_article_list);
 
-        public Adapter(Cursor cursor) {
-            mCursor = cursor;
-        }
+      mToolbar = (Toolbar) findViewById(R.id.toolbar);
 
-        @Override
-        public long getItemId(int position) {
-            mCursor.moveToPosition(position);
-            return mCursor.getLong(ArticleLoader.Query._ID);
-        }
+      //        final View toolbarContainerView = findViewById(R.id.toolbar_container);
 
-        @Override
-        public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-            View view = getLayoutInflater().inflate(R.layout.list_item_article, parent, false);
-            final ViewHolder vh = new ViewHolder(view);
-            view.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View view) {
-                    startActivity(new Intent(Intent.ACTION_VIEW,
-                            ItemsContract.Items.buildItemUri(getItemId(vh.getAdapterPosition()))));
-                }
-            });
-            return vh;
-        }
+      mSwipeRefreshLayout = (SwipeRefreshLayout) findViewById(R.id.swipe_refresh_layout);
 
-        @Override
-        public void onBindViewHolder(ViewHolder holder, int position) {
-            mCursor.moveToPosition(position);
-            holder.titleView.setText(mCursor.getString(ArticleLoader.Query.TITLE));
-            holder.subtitleView.setText(
-                    DateUtils.getRelativeTimeSpanString(
-                            mCursor.getLong(ArticleLoader.Query.PUBLISHED_DATE),
-                            System.currentTimeMillis(), DateUtils.HOUR_IN_MILLIS,
-                            DateUtils.FORMAT_ABBREV_ALL).toString()
-                            + " by "
-                            + mCursor.getString(ArticleLoader.Query.AUTHOR));
-            holder.thumbnailView.setImageUrl(
-                    mCursor.getString(ArticleLoader.Query.THUMB_URL),
-                    ImageLoaderHelper.getInstance(ArticleListActivity.this).getImageLoader());
-            holder.thumbnailView.setAspectRatio(mCursor.getFloat(ArticleLoader.Query.ASPECT_RATIO));
-        }
+      mRecyclerView = (RecyclerView) findViewById(R.id.recycler_view);
+      getLoaderManager().initLoader(0, null, this);
 
-        @Override
-        public int getItemCount() {
-            return mCursor.getCount();
-        }
-    }
+      if (savedInstanceState == null) {
+         refresh();
+      }
+   }
 
-    public static class ViewHolder extends RecyclerView.ViewHolder {
-        public DynamicHeightNetworkImageView thumbnailView;
-        public TextView titleView;
-        public TextView subtitleView;
+   @Override
+   protected void onStart() {
+      super.onStart();
+      registerReceiver(mRefreshingReceiver,
+            new IntentFilter(UpdaterService.BROADCAST_ACTION_STATE_CHANGE));
+   }
 
-        public ViewHolder(View view) {
-            super(view);
-            thumbnailView = (DynamicHeightNetworkImageView) view.findViewById(R.id.thumbnail);
-            titleView = (TextView) view.findViewById(R.id.article_title);
-            subtitleView = (TextView) view.findViewById(R.id.article_subtitle);
-        }
-    }
+   @Override
+   protected void onStop() {
+      super.onStop();
+      unregisterReceiver(mRefreshingReceiver);
+   }
+
+   private void refresh() {
+      startService(new Intent(this, UpdaterService.class));
+   }
+
+   private void updateRefreshingUI() {
+      mSwipeRefreshLayout.setRefreshing(mIsRefreshing);
+   }
 }
