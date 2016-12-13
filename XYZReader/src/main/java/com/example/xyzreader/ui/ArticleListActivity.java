b6a9b44b4a11067ececec19354e974537d8b1952
Michael Lorenz <michael.lorenz@intive-kupferwerk.com>
2016-12-13 12:09:27 +0100

    some styling

--- XYZReader/src/main/java/com/example/xyzreader/ui/ArticleListActivity.java
+++ XYZReader/src/main/java/com/example/xyzreader/ui/ArticleListActivity.java
@@ -9,7 +9,7 @@ import android.content.Loader;
 import android.database.Cursor;
 import android.os.Bundle;
 import android.support.v4.widget.SwipeRefreshLayout;
-import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.StaggeredGridLayoutManager;
 import android.support.v7.widget.Toolbar;
@@ -29,7 +29,7 @@ import com.example.xyzreader.data.UpdaterService;
  * touched, lead to a {@link ArticleDetailActivity} representing item details. On tablets, the
  * activity presents a grid of items as cards.
  */
-public class ArticleListActivity extends ActionBarActivity
+public class ArticleListActivity extends AppCompatActivity
       implements LoaderManager.LoaderCallbacks<Cursor> {
 
    public static class ViewHolder extends RecyclerView.ViewHolder {
@@ -98,6 +98,7 @@ public class ArticleListActivity extends ActionBarActivity
    private BroadcastReceiver mRefreshingReceiver = new BroadcastReceiver() {
       @Override
       public void onReceive(Context context, Intent intent) {
+
          if (UpdaterService.BROADCAST_ACTION_STATE_CHANGE.equals(intent.getAction())) {
             mIsRefreshing = intent.getBooleanExtra(UpdaterService.EXTRA_REFRESHING, false);
             updateRefreshingUI();
@@ -137,7 +138,12 @@ public class ArticleListActivity extends ActionBarActivity
       //        final View toolbarContainerView = findViewById(R.id.toolbar_container);
 
       mSwipeRefreshLayout = (SwipeRefreshLayout) findViewById(R.id.swipe_refresh_layout);
-
+      mSwipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+         @Override
+         public void onRefresh() {
+            refresh();
+         }
+      });
       mRecyclerView = (RecyclerView) findViewById(R.id.recycler_view);
       getLoaderManager().initLoader(0, null, this);
 
